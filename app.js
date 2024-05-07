0696dea4f6c04ee0b3c01402e119a00582fb9037
syedgulzar786 <syedgulzarmohammad123@gmail.com>
2024-05-08 02:57:33 +0500

    assignment 8

---
+++ app.js
@@ -0,0 +1,73 @@
+// // var count = 0;
+// function myValue(){
+// var count = 0;
+//     for(var i = 0; i < 5; i++){
+//         count++;
+// console.log(i, count)
+//     }
+// //  var val = document.getElementById("username").value
+// //  console.log(val)
+// }
+// myValue()
+// myValue()
+// myValue()
+
+// function foo(a, b){
+//   console.log(a, b)
+// }
+// foo(2, 2)
+
+// function foo(notify){
+//     console.log(notify())
+//   }
+//   foo(alert)
+
+//   function foo(notify){
+//     console.log(notify)
+//   }
+//   var arr = ["smith"]
+//   foo(arr[0])
+
+// function checkAttendance (bacha){
+//     var list = [1122, 2233, 3344, 4455]
+//     if(list.indexOf(bacha) !== -1){
+//         return "aya!"
+//     }
+//     else{
+//         return "nhi aya!"
+//     }
+
+// }
+// console.log(checkAttendance(1122),checkAttendance(2223),checkAttendance(3344),checkAttendance(4455))
+
+// var count = 1;
+// function foo(){
+//     for(var i = 0; i < 3; i++){
+//           count++
+//           console.log(count)
+//           return count
+//     }
+// }
+//     console.log(foo())
+
+
+// function checkAttendance (bacha){
+//     var list = [1122, 2233, 3344, 4455]
+//     if(list.indexOf(bacha) !== -1){
+//         return "enrolled!"
+//     }
+//     else{
+//         return "eliminated!"
+//     }
+
+// }
+// var check = [1122, 2223, 3344, 4655]
+// for(var i = 0; i < check.length; i++){
+//         console.log(checkAttendance(check[i]))
+// }
+// // console.log(checkAttendance(1122),checkAttendance(2223),checkAttendance(3344),checkAttendance(4455))
+
+// function foo(a, b){
+//     return a * b
+// }
+// console.log(foo(1, 2))
